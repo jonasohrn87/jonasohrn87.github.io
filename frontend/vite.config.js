f98f8783c68def6fef67c2aa707271b3890ab144
jonasohrn87 <145778083+jonasohrn87@users.noreply.github.com>
2025-09-11 12:53:37 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 
+const repoName = "/"
+const isProjectSite = true
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: isProjectSite ? `/${repoName}/` : '/',
 })
+
